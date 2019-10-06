534734d2e7c09b303029b6112d8d4860e078f11f
tdmhang <tdmhang@qq.comm>
2019-10-06 23:20:19 +0800

    Updates

---
+++ precache-manifest.188986490466a064a038cd4a4fd8a33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2229e4dae96945d9e6bdca908db65fb6",
+    "url": "/beat1/index.html"
+  },
+  {
+    "revision": "a17bf7558aa0d1ed14d4",
+    "url": "/beat1/static/css/main.2d6217bc.chunk.css"
+  },
+  {
+    "revision": "95b75294e34816007430",
+    "url": "/beat1/static/js/2.e43e62d1.chunk.js"
+  },
+  {
+    "revision": "a17bf7558aa0d1ed14d4",
+    "url": "/beat1/static/js/main.0cef3d2a.chunk.js"
+  },
+  {
+    "revision": "03abbe9eee74fb21f344",
+    "url": "/beat1/static/js/runtime-main.9e897ac8.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/beat1/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/beat1/static/media/click2.fe0ded65.wav"
+  }
+]);
